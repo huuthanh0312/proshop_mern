900bf67dc00abea38cafd1b6d09329115cd2ef32
huuthanh0312 <huuthanhnguyen0312@gmail.com>
2024-05-13 02:46:13 +0700

    React frontend Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import Header from "./compoments/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./compoments/Footer";
+import { Outlet } from "react-router-dom";
 const App = () => {
-  return <div>Wel Come To ThanhShop</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
